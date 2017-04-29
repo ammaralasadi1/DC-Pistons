977b26065a43410431630dfad6c1faa02b6f34bc
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-04-28 22:28:57 -0400

    CSS Stable

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,34 @@ Comment.destroy_all
 
 art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
 comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
+
+art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
+comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
+
+art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
+comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
+
+art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
+comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
+
+art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
+comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
+
+art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
+comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
+
+
+art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
+comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
+
+art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
+comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
+
+art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
+comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
+
+art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
+comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
+
+art_one = Article.create(title: "petrol", body: "Loremhhhhdhdhsdahadhasdhasdhdhsadsadsadsad", author_name: "Alex Mandels", photo_url: "https://goo.gl/uDZhbj")
+comment = Comment.create(body:"Hi", author_name: "yes", article_id: 1)
