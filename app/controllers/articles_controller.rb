f8c09427b29a6a449cceccfa850f027408bf9b5c
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-05-01 16:33:47 -0400

    Category selection stable

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
   # index
   def index
+    if params[:category].blank?
     @articles = Article.all
+  else
+    category_id = Category.find_by(name: params[:category]).id
+    @articles = Article.where(:category_id => category_id).order("created_at DESC")
+  end
   end
 
   # new
