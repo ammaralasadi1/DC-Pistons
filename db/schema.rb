73a1923fe842c186d1fbd164296c5d817e449d9e
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-05-02 20:32:09 -0400

    Style: Coming along

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502140206) do
+ActiveRecord::Schema.define(version: 20170501172324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20170502140206) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.integer  "category_id"
     t.index ["user_id"], name: "index_cars_on_user_id", using: :btree
   end
 
